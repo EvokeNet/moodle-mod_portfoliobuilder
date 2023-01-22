004e4dee3e3844062b65464c3104e7836111fb2d
Willian Mano <willianmanoaraujo@gmail.com>
2023-01-22 17:01:49 -0300

    Increase version num

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_portfoliobuilder';
 $plugin->release = '1.0.0';
-$plugin->version = 2023011500;
+$plugin->version = 2023012000;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;
