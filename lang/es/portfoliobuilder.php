06dc4131e2fcfdad5621ae3398c5939b8cb79674
Willian Mano <willianmanoaraujo@gmail.com>
2023-01-24 21:03:57 -0300

    Add ES lang

---
+++ lang/es/portfoliobuilder.php
@@ -0,0 +1,109 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_portfoliobuilder
+ * @category    string
+ * @copyright   2022 Willian Mano <willianmanoaraujo@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'Falta id y cmid';
+$string['missingportfoliowithevaluation'] = 'Este curso no tiene portfolio con evaluación.';
+$string['modulename'] = 'Portafolio individual';
+$string['modulename_help'] = 'Usar el módulo de portafolio para...';
+$string['modulenameplural'] = 'Portafolios';
+$string['newmodulesettings'] = 'Configuración';
+
+$string['noportfolios'] = 'Sin portafolios';
+$string['pluginadministration'] = 'Administración de la portafolio';
+$string['pluginname'] = 'Portafolio individual';
+$string['portfoliobuilder:addinstance'] = 'Agregar instancia';
+$string['portfoliobuilder:view'] = 'Ver instancia';
+$string['portfoliobuilder:submit'] = 'Enviar entradas de portafolio';
+$string['portfoliobuilder:grade'] = 'Calificar portafolio';
+$string['privacy:metadata'] = 'La portafolio no almacena ningún dato personal';
+$string['view'] = 'Ver';
+
+$string['name'] = 'Nombre';
+$string['title'] = 'Título';
+$string['content'] = 'Contenido';
+$string['attachments'] = 'Archivos adjuntos';
+$string['validation:titlerequirelen'] = 'El título es obligatorio y debe tener al menos 3 caracteres.';
+$string['validation:contentachmentsrequired'] = 'Debe agregar texto a su entrada o enviar al menos un archivo.';
+$string['validation:contentlen'] = 'El texto de la publicación debe tener al menos 10 caracteres';
+
+$string['entry:add'] = 'Agregar una nueva entrada a su portafolio';
+$string['entry:add_success'] = 'Entrada creada con éxito';
+$string['entry:update_success'] = 'Entrada actualizada con éxito';
+
+$string['downloadfile'] = 'Descargar archivo';
+$string['linkccopiedtoclipboard'] = 'Enlace copiado al portapapeles';
+
+$string['backtolayoutchoose'] = 'Volver a elegir diseño';
+
+$string['viewportfolio'] = 'Ver portafolio';
+$string['comment'] = 'Comentario';
+$string['writeacomment'] = '+ Comparte tus pensamientos...';
+$string['editcomment'] = 'Editar';
+$string['editcomment_success'] = 'Comentario editado con éxito.';
+$string['edited'] = 'Editado';
+
+$string['myteam'] = 'Mi equipo';
+$string['myportfolios'] = 'Mis Portafolios';
+$string['myportfolio'] = 'Mi Portafolio';
+$string['teamportfolio'] = 'Portafolio del equipo';
+$string['networkportfolio'] = 'Portafolio de red';
+
+$string['grade'] = 'Calificación';
+$string['addgrade'] = 'Agregar calificación';
+$string['assessment'] = 'Evaluación';
+$string['notyetassessed'] = 'Aún no evaluado';
+$string['grading_success'] = 'Calificación finalizada con éxito.';
+
+$string['status'] = 'Estado';
+$string['submitted'] = 'Enviado';
+$string['evaluated'] = 'Evaluado';
+$string['groups'] = 'Grupos';
+
+$string['previous'] = 'Anterior';
+$string['next'] = 'Siguiente';
+$string['viewtablereport'] = 'Ver informe de tabla';
+$string['timeline'] = 'Línea de tiempo';
+$string['mansory'] = 'Mansory';
+$string['blog'] = 'Blog';
+$string['previewthislayout'] = 'Vista previa de este diseño';
+$string['savemypreferences'] = 'Guardar mis preferencias';
+$string['chooselayouttitle'] = 'Estimado estudiante, antes de comenzar a crear su portafolio, primero debe elegir un diseño para él.';
+$string['chooselayoutsubtitle1'] = 'Puede hacer clic en los botones de vista previa para ver cómo se presenta cada diseño.';
+$string['chooselayoutsubtitle2'] = 'No te preocupes, puedes cambiar el diseño de tu Portafolio cuando quieras.';
+$string['backtoportfolios'] = 'Volver a los portafolios';
+$string['hellothisismyportfolio'] = '¡Hola, este es mi portafolio!';
+$string['portfolioempty'] = '¡Portafolio vacío!';
+$string['portfolioemptydesc'] = 'Todavía no hay entradas para este portafolio, vuelva más tarde.';
+$string['yourportfolioisempty'] = 'Su portafolio está vacío.';
+$string['yourportfolioisemptydesc'] = 'Estimado estudiante, debe comenzar a crear su carpeta, haga clic en el botón de arriba para comenzar a agregarle entradas.';
+$string['news'] = 'NOTICIAS';
+$string['lastentry'] = 'Última entrada';
+$string['entries'] = 'Entradas';
+$string['likes'] = 'Me gusta';
+$string['comments'] = 'Comentarios';
+$string['shareportfolio'] = 'Portafolio de acciones';
+$string['changelayout'] = 'Cambiar diseño';
